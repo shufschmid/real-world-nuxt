cde60a574742c6f81156ac6140c5fe997cea4912
shufschmid <shufschmid@gmail.com>
2021-05-16 17:47:57 +0200

    code to deploy on netlify

---
+++ pages/event/create.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Create Event</h1>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create Events',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'create best events in town',
+        },
+      ],
+    }
+  },
+}
+</script>
